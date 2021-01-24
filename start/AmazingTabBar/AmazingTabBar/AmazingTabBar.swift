714ff03d456abab345ef355a4365e25147e59ebe
NikitaFilonov <iamnikitafilonov@gmail.com>
2021-01-24 12:19:59 +0300

    start

---
+++ start/AmazingTabBar/AmazingTabBar/AmazingTabBar.swift
@@ -0,0 +1,14 @@
+//
+//  AmazingTabBar.swift
+//  AmazingTabBar
+//
+//  Created by Dragonborn on 24.01.2021.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class AmazingTabBar: UITabBar {
+    
+}
